c1ada8a1defae24d647a64b85c30182c5402a626
Antonio Cambule <antonio@camedia.de>
2023-08-14 14:46:32 +0200

    change and push to main

---
+++ actions-demo.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package camedia-support
+ */
+/*
+Plugin Name: GitHub Actions Test
+Plugin URI: github.com/camediaonline
+Description: DUMMY Plugin for Github Action Testing
+Version: 1.0.0
+Author: CAMEDIA GmbH
+Author URI: https://camedia.de
+License: MIT
+*/
+
+// If this file is called directly, abort!!!
+defined('ABSPATH') or die('No access in here! Why did you even try?');
